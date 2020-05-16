d78ba5b564a6ef44bac929f05b2dc11f96f9b9b4
Ryan Schueck <ryschueck@gmail.com>
2020-05-15 21:16:52 -0400

    finished MVP for coding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import PizzaForm from "./components/PizzaForm.js"
+import { Route, Link } from "react-router-dom"
 
 const App = () => {
   return (
-    <>
+    <div>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/pizza">Order Here</Link>
+      </nav>
+      <Route exact path="/" component={App}/>
+      <Route path="/pizza" component={PizzaForm} />
+    </div>
   );
 };
 export default App;
